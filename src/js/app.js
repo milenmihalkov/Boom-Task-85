0f725e056d58fae4244a40e4ef55f875701d412e
Milen <milenmihalkov@gmail.com>
2023-01-29 23:28:43 +0200

    pokemon

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,27 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const apiUrl  = 'https://pokeapi.co/api/v2/pokemon';
+  const ul      =  document.querySelector("ul");
+  
+  
+async function getData(){
 
-  const ul = document.querySelector("ul");
+}
+  fetch(apiUrl)
+    .then(response => {
+
+        let data = response.json();
+
+        for(let i = 0; i<data.length; i++){
+          let listItem  =  document.createElement('li');
+          ul.appendChild(listItem);
+        }
+        console.log(data);
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
+  
 });
