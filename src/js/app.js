b5dd3fef9f9bd64764db1a8690279f2e8f8f45c2
Milen <milenmihalkov@gmail.com>
2023-01-30 10:16:43 +0200

    dzfgh

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,12 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const pokemonCount = 10;
-  // const apiUrl  = ;
+  const apiUrl  = `https://pokeapi.co/api/v2/pokemon/?limit=${pokemonCount}`;
   const ul =  document.querySelector("ul");
   
   
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${pokemonCount}`)
+  fetch(apiUrl)
     .then((response) => response.json()).then(data => {
 
         data.results.forEach((pokemon) => {
